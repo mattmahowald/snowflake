fd28e970214fec611c693d30352319adf3050b76
Matthew Knopp <matt@modeanalytics.com>
2019-01-07 13:30:13 -0800

    Updates

--- _next/326d0aed-6228-4d59-a3ad-5878a6f36217/page/index.js
+++ _next/326d0aed-6228-4d59-a3ad-5878a6f36217/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[225]);
+            var comp = module.exports=webpackJsonp([2],[],[226]);
             return { page: comp.default }
           })
         
